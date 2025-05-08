2829b73ccfa7d6e21c51084874080edb2b08cf79
jhonzanxyz <jhonzanxyz@gmail.com>
2025-05-08 15:37:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 # Config Variables from environment or fallback
 TOKEN = os.getenv('7976536059:AAFui0l-RQTkjFV3yl58MOkZRpXw4a_av5I')
 OWNER_ID = int(os.getenv('6444269766'))
-FORCE_SUB_CHANNEL = os.getenv('FORCE_SUB_CHANNEL')
+FORCE_SUB_CHANNEL = os.getenv('@magtorn')
